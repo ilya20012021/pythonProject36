65af3f0df54846790f1f58c487d21b415cf69b1e
Илья <moskalevilya1@gmail.com>
2022-12-21 02:09:20 +0300

    Конец

--- main.py
+++ main.py
@@ -11,58 +11,72 @@ n = 1
 
 while(n != 0):
     print("Вас приветствует Автоматизированная информационно-аналитическая система google search")
-    a = ["да", "нет"]
-    b = "/".join(a)
-    print(f"Новый запрос?{b}")
-    print("Если нужна работа с уже имеющимися таблицами,также можете нажать ENTER")
-    ans = str(input())
-    if(ans == a[0]):
-        theme = str(input("Введите тему запроса:"))
-        count = int(input("Введите кол-во ссылок:"))
-        res1 = search(theme, stop=count)
-        res = list(res1)
-        id = [int(i) for i in range(len(res))]
-        work = []
-        nums = [0 for i in range(len(res))]
-        for i in res:
-            try:
-                r = requests.get(i)
-                work.append("+")
-            except requests.exceptions.ConnectionError:
-                work.append("-")
-            except requests.exceptions.ReadTimeout:
-                work.append("-")
-        df = pd.DataFrame({})
-        df["id"] = id
-        df["results"] = res
-        df["work"] = work
-        df["nums"] = nums
-        df1 = df.to_json()
-        df2 = json.loads(df1)
-        print(json.dumps(df2,indent=len(df2)))
-        df3 = json.dumps(df2,indent=len(df2))
-        name = str(input("Введите имя таблицы:"))
-        with open("1.txt", "a+") as f:
-            f.write(f'{name}\n')
-        with open(f"{name}.json", "a+") as f:
-            f.write(df3)
-    dir = ["Получить доступ к таблицам(нажмите 1)","Нарисовать диаграмму(нажмите 2)"]
-    dir1 = ",".join(dir)
-    print(dir1)
-    r = str(input("Введите ответ:"))
-    if(r == "1"):
-        data.get()
-    elif(r == "2"):
-        diagram.draw()
+    login = ["Il","Milka"]
+    passwd = ["Mos","Mila"]
+    h1 = str(input("Введите логин:"))
+    h2 = str(input("Введите пароль:"))
+    if(h1 in login and h2 in passwd):
+        if(login.index(h1) == passwd.index(h2)):
+            a = ["да", "нет"]
+            b = "/".join(a)
+            print(f"Новый запрос?{b}")
+            print("Если нужна работа с уже имеющимися таблицами,также можете нажать ENTER")
+            ans = str(input())
+            if (ans == a[0]):
+                theme = str(input("Введите тему запроса:"))
+                count = int(input("Введите кол-во ссылок:"))
+                res1 = search(theme, stop=count)
+                res = list(res1)
+                id = [int(i) for i in range(len(res))]
+                work = []
+                nums = [0 for i in range(len(res))]
+                for i in res:
+                    try:
+                        r = requests.get(i)
+                        work.append("+")
+                    except requests.exceptions.ConnectionError:
+                        work.append("-")
+                    except requests.exceptions.ReadTimeout:
+                        work.append("-")
+                df = pd.DataFrame({})
+                df["id"] = id
+                df["results"] = res
+                df["work"] = work
+                df["nums"] = nums
+                df1 = df.to_json()
+                df2 = json.loads(df1)
+                print(json.dumps(df2, indent=len(df2)))
+                df3 = json.dumps(df2, indent=len(df2))
+                name = str(input("Введите имя таблицы:"))
+                with open("1.txt", "a+") as f:
+                    f.write(f'{name}\n')
+                with open(f"{name}.json", "a+") as f:
+                    f.write(df3)
+            dir = ["Получить доступ к таблицам(нажмите 1)", "Нарисовать диаграмму(нажмите 2)"]
+            dir1 = ",".join(dir)
+            print(dir1)
+            r = str(input("Введите ответ:"))
+            if (r == "1"):
+                data.get()
+            elif (r == "2"):
+                diagram.draw()
+            else:
+                print("???")
+            ans = ["да", "нет"]
+            ans_n = "/".join(ans)
+            print(f"Хотите ли вы продолжить работу с данным ПО?{ans_n}")
+            s = str(input("Введите ответ:"))
+            if (s == ans[0]):
+                n += 1
+            elif (s == ans[1]):
+                break
+            else:
+                print("Ничего непонятно")
+
+        else:
+            print("Неверные данные!")
+            break
+
     else:
-        print("???")
-    ans = ["да", "нет"]
-    ans_n = "/".join(ans)
-    print(f"Хотите ли вы продолжить работу с данным ПО?{ans_n}")
-    s = str(input("Введите ответ:"))
-    if (s == ans[0]):
-        n += 1
-    elif (s == ans[1]):
+        print("Неверные данные!")
         break
-    else:
-        print("Ничего непонятно")
